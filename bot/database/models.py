c4b7ac23452af92d6e04784649792708f8f881c5
Angelina Key <angelina@example.com>
2025-04-23 17:10:58 +0300

    Удалены импорты async SQLAlchemy

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 from datetime import datetime
 import os
@@ -17,24 +16,3 @@ class Message(Base):
     answer = Column(Text)
     timestamp = Column(DateTime, default=datetime.utcnow)
 
-# Подключение к БД
-DB_URL = os.getenv("DB_URL")
-
-engine = create_async_engine(DB_URL, echo=False)
-async_session = async_sessionmaker(engine, expire_on_commit=False)
-
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-# async def log_interaction(user_id, age, experience, question, answer):
-#     async with async_session() as session:
-#         msg = Message(
-#             user_id=user_id,
-#             age=age,
-#             experience=experience,
-#             question=question,
-#             answer=answer
-#         )
-#         session.add(msg)
-#         await session.commit()
